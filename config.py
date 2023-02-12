67192256818e8db99e92c280cb3293558051013f
Ankur12321 <84258110+Ankur12321@users.noreply.github.com>
2023-02-12 23:56:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", "12124605"))
-API_HASH = os.environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5574279873:AAHb22nEOdC2dbwY_9GpU838frtUAvyFoQI")
+API_ID = int(os.environ.get("API_ID", "21970746"))
+API_HASH = os.environ.get("API_HASH", "32deb816dc3874e871b6158673fd3683")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5827692137:AAHXOHrODrK_8faFr_E1q5_txpXQ1ZAhmGc")
 DROPLINK_API = os.environ.get("DROPLINK_API")
-MDISK_API = os.environ.get("MDISK_API", "Qu7jX9V0Sn3q1JHdxjPp")
+MDISK_API = os.environ.get("MDISK_API", "ed8f24501bcea56baafbd8a311be1ccb98bb0c2e")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertorBotpro")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Ankur2:ankur1234321@cluster0.7fkfy9z.mongodb.net/?retryWrites=true&w=majority")
 WEBSITE = os.environ.get('WEBSITE')
 
 #  Optionnal variables
